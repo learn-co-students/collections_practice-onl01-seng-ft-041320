9b2df6efe4b9be07d6d3c2f4365d3a4f8768e2f5
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-16 16:25:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
-
+def sort_array_asc (array)
+  array.sort
+end
+def sort_array_desc (array)
+  array.sort {|a, b| b<=>a}
+end
+def sort_array_char_count (array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+def swap_elements (array)
+  swap = array[1]
+  array[1] = array[2]
+  array[2] = swap
+  array
+end
+def reverse_array (array)
+  array.reverse!
+end
+def kesha_maker (array)
+  array.each {|element| element[2] = "$"}
+end
+def find_a (array)
+  array.select {|string| string.start_with?("a")}
+end
+def sum_array (array)
+  array.inject {|sum, n| sum + n}
+end
+def add_s (array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element << "s" 
+    else
+      element
+    end
+  end
+end
